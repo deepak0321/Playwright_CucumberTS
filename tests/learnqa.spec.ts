89e0e111b25e1b63bb582f7e55d658858d96e903
Deepak Raj Gnanamuralidharan(UST <291493@ust.com>
2026-02-23 12:41:32 +0530

    changes to handle env variables in CI

--- tests/learnqa.spec.ts
+++ tests/learnqa.spec.ts
@@ -19,8 +19,8 @@ test.describe('LearnQA Tests', () => {
         await page.goto('https://www.learnaqa.info/');
         await page.getByRole('button', { name: 'Accept All' }).click();
         await page.getByRole('button', { name: 'Sign In' }).nth(1).click();
-        await page.getByPlaceholder('Enter your email').fill(process.env.email!);
-        await page.getByLabel('Password').fill(process.env.password!);
+        await page.getByPlaceholder('Enter your email').fill(process.env.EMAIL!);
+        await page.getByLabel('Password').fill(process.env.PASSWORD!);
         await page.getByRole('button', { name: 'Sign In' }).click();
         await expect(page.getByRole('status')).toHaveText('Successfully logged in!');
     });
